b011916007a7112bb32778894e4a110f30105b0b
Victor is Geek <92975790+victorgeel@users.noreply.github.com>
2023-06-21 22:18:58 +0630

    Create geek.is-an.app.js

---
+++ domains/geek.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({ 
+   description: 'A Fast, Lightweight and Modern native text editor.', 
+   domain: 'is-an.app', 
+   subdomain: 'geek', 
+   owner: { 
+     repo: 'https://github.com/victorgeel/GCP-server', 
+     email: 'freevpn537@gmail.com', 
+   }, 
+   record: { 
+     CNAME: 'victorgeel.github.io', // e.g.: <your-github-account>.github.io 
+   }, 
+ })
